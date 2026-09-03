23fce48c59444fb1cf0e49f26aeb3dffb5fbf1f5
Marya Martins <marya60561086@alunos.to.senac.br>
2026-09-03 14:34:28 -0300

    mensagem

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+// comentário de linha só
+/*
+ comentário
+de várias
+linhas
+*/
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author MaryaMartins
+ */
+public class Variaveis {
+
+    public static void main(String[] args) {
+ /* código de saída
+        - são utilizado para imprimir/mostrar
+        dados e mensagens
+        */
+        System.out.println("olá");
+        System.out.println ("eu sou uma programdora");
+        
+        
+        // printl - pula linha / print - não pula
+        System.out.print("Eu ja sei programar");
+        System.out.print ("eu amo java");
+        
+        // CRIAÇAO  DE VARIÁVEIS 
+        int idade = 24;
+        double altura = 1.60;
+        float peso = 58;
+        String nome = "Marya Klara";
+        boolean solteira = false;
+        // true - verdadeiro , false - falso
+         System.out.println( "olá"+nome)
+         System.out.printl( "voce tem "+" anos de idade")
+    }// fim da main
+}
+// nenhum código pode ficar aqui fora
